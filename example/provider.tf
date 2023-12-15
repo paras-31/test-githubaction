1e7dcbc1d9b0b52cc510c3699561b2cdd6ca284b
Paras Kamboj <paras.kamboj@cloudeq.com>
2023-12-15 20:29:02 +0530

    new

--- example/provider.tf
+++ example/provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIARHUMF54XXP37EFMW"
-  secret_key = "Hu3iJ/r7QNGGu/G41/ntqT32bSCnyfqcsynD5RkT"
+  access_key = "AKIARHUMF54XVO62OVVN"
+  secret_key = "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
+"
 }
