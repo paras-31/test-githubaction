4e04c5bda386cfebbf677d37aa45bc819552b01b
Paras Kamboj <paras.kamboj@cloudeq.com>
2023-12-15 20:27:06 +0530

    new

--- example/main.tf
+++ example/main.tf
@@ -3,15 +3,6 @@ resource "aws_instance" "example" {
   ami           = "ami-06791f9213cbb608b" # Amazon Linux 2 AMI ID
   instance_type = "t2.micro"
   subnet_id     = "	subnet-0b3fd8cdc676642fb" # Use the appropriate subnet
-  user_data     = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y httpd 
-              sudo systemctl start httpd
-              sudo systemctl enable httpd
-              sudo su
-              echo "<h1> hello world from $(hostname -f)</h1>" > /var/www/html/index.html
-              EOF
   associate_public_ip_address = true # Associates a public IPv4 address
 
   tags = {
