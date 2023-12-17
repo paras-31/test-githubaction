ea895f9e57feaf95348ebc9acd78242e22773b88
Paras Kamboj <paras.kamboj@cloudeq.com>
2023-12-17 14:00:26 +0530

    code pushed

--- example/provider.tf
+++ example/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIARHUMF54XVO62OVVN"
-  secret_key = "YnDtJAeOJmrLRAv9zhRETAQo24yVIN5l8/ymE8b1"
+  access_key = "AKIARHUMF54XWQMLI7GC"
+  secret_key = "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"
 }
