a35ecc41c581ce9876d5ea4a91fe4b10ac0e9d97
Paras Kamboj <paras.kamboj@cloudeq.com>
2023-12-19 13:09:42 +0530

    pushing and please run222

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   count = 2
-  ami           = "ami-06791f9213cbb608b" # Amazon Linux 2 AMI ID
+  ami           = "ami-0a0f1259dd1c90938" # Amazon Linux 2 AMI ID
   instance_type = "t2.micro"
   subnet_id     = "subnet-0b3fd8cdc676642fb" # Use the appropriate subnet
   associate_public_ip_address = true # Associates a public IPv4 address
