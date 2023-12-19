c5513bdf1fe1f3e955bec18c61cd9205c1fe78b1
Paras Kamboj <paras.kamboj@cloudeq.com>
2023-12-19 13:03:18 +0530

    run

--- example/main.tf
+++ example/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "example" {
   count = 2
   ami           = "ami-06791f9213cbb608b" # Amazon Linux 2 AMI ID
   instance_type = "t2.micro"
-  subnet_id     = "	subnet-0b3fd8cdc676642fb" # Use the appropriate subnet
+  subnet_id     = "subnet-0b3fd8cdc676642fb" # Use the appropriate subnet
   associate_public_ip_address = true # Associates a public IPv4 address
 
   tags = {
--- example/provider.tf
+++ example/provider.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
-  access_key = "AKIARHUMF54XWQMLI7GC"
-  secret_key = "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"
+  region = "us-east-1"
 }
