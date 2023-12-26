cfa8d3cc59c8ab990a8278bfd3eb204744fb3171
Paras Kamboj <paras.kamboj@cloudeq.com>
2023-12-26 22:10:35 +0530

    update51

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_key_pair" "tf-key-pair" {
-  key_name   = "tf-key-pair5"
+  key_name   = "tf-key-pair51"
   public_key = tls_private_key.rsa.public_key_openssh
 }
 resource "tls_private_key" "rsa" {
@@ -9,7 +9,7 @@ resource "tls_private_key" "rsa" {
 }
 resource "local_file" "tf-key" {
   content  = tls_private_key.rsa.private_key_pem
-  filename = "tf-key-pair5"
+  filename = "tf-key-pair51"
 }
 
 resource "aws_instance" "example" {
