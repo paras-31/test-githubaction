af6523f8a103dedffd79129fdd404fea33db7066
Paras Kamboj <paras.kamboj@cloudeq.com>
2023-12-26 21:30:03 +0530

    update in blank2

--- ec2/main.tf
+++ ec2/main.tf
@@ -12,19 +12,15 @@ resource "local_file" "tf-key" {
   filename = "tf-key-pair"
 }
 
-
-
 resource "aws_instance" "example" {
-  count = 2
-  ami           = "ami-074f77adfeee318d3" # Amazon Linux 2 AMI ID
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-0b3fd8cdc676642fb" # Use the appropriate subnet
-  associate_public_ip_address = true # Associates a public IPv4 address
-
+  ami                         = "ami-074f77adfeee318d3" # Amazon Linux 2 AMI ID
+  instance_type               = "t2.micro"
+  subnet_id                   = "subnet-0b3fd8cdc676642fb" # Use the appropriate subnet
+  associate_public_ip_address = true
+  #   key_name                    = "tf-key-pair"
   key_name = aws_key_pair.tf-key-pair.key_name
 
-
   tags = {
-    Name = "instance${count.index + 1}"
-  }   
+    Name = "example-instance"
+  }
 }
