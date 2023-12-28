5071790b5a4a3f0eb2d570af7c5e003948472de1
Paras Kamboj <paras.kamboj@cloudeq.com>
2023-12-28 22:36:42 +0530

    115-new94

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_key_pair" "tf-key-pair" {
-  key_name   = "tf-key-pair95"
+  key_name   = "tf-key-pair96"
   public_key = tls_private_key.rsa.public_key_openssh
 }
 resource "tls_private_key" "rsa" {
@@ -9,7 +9,7 @@ resource "tls_private_key" "rsa" {
 }
 resource "local_file" "tf-key" {
   content  = tls_private_key.rsa.private_key_pem
-  filename = "tf-key-pair95"
+  filename = "tf-key-pair96"
 }
 
 resource "aws_instance" "example" {
